62dd377ca965867fdbdf1ebca61afdbc85c6db87
David Shapiro <daveshap37@gmail.com>
2023-09-21 23:53:29 -0400

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,54 @@
+import re
+import openai
+from time import time, sleep
+from halo import Halo
+import textwrap
+import yaml
+
+
+###     file operations
+
+
+def save_file(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
+
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8', errors='ignore') as infile:
+        return infile.read()
+
+
+###     API functions
+
+
+def chatbot(conversation, model="gpt-4-0613", temperature=0, max_tokens=2000):
+    max_retry = 7
+    retry = 0
+    while True:
+        try:
+            spinner = Halo(text='Thinking...', spinner='dots')
+            spinner.start()
+            
+            response = openai.ChatCompletion.create(model=model, messages=conversation, temperature=temperature, max_tokens=max_tokens)
+            text = response['choices'][0]['message']['content']
+
+            spinner.stop()
+            
+            return text, response['usage']['total_tokens']
+        except Exception as oops:
+            print(f'\n\nError communicating with OpenAI: "{oops}"')
+            exit(5)
+
+
+def chat_print(text):
+    formatted_lines = [textwrap.fill(line, width=120, initial_indent='    ', subsequent_indent='    ') for line in text.split('\n')]
+    formatted_text = '\n'.join(formatted_lines)
+    print('\n\n\nCHATBOT:\n\n%s' % formatted_text)
+
+
+if __name__ == '__main__':
+    openai.api_key = open_file('key_openai.txt').strip()
+    convo = list()
+    
